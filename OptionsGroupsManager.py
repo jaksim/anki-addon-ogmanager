65f7352f095f0f9f15017a044f3db1ef755d58e0
Jakub Simek <jakub.simek@hotmail.com>
2014-02-09 17:04:25 +0100

    decided to stuff it all into a single file

---
+++ OptionsGroupsManager.py
@@ -0,0 +1,78 @@
+from aqt import mw
+from anki.qt import *
+from PyQt4 import QtCore, QtGui
+
+class OGManager(QDialog):
+
+    def __init__(self, mw):
+        QDialog.__init__(self)
+
+        self.mw = mw
+
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
+    def show(self):
+        self.show()
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName(_fromUtf8("Dialog"))
+        Dialog.resize(504, 484)
+        Dialog.setMinimumSize(QtCore.QSize(400, 300))
+        self.verticalLayout = QtGui.QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.label = QtGui.QLabel(Dialog)
+        self.label.setTextFormat(QtCore.Qt.AutoText)
+        self.label.setScaledContents(False)
+        self.label.setWordWrap(True)
+        self.label.setObjectName(_fromUtf8("label"))
+        self.verticalLayout.addWidget(self.label)
+        self.treeWidget = QtGui.QTreeWidget(Dialog)
+        self.treeWidget.setAnimated(True)
+        self.treeWidget.setHeaderHidden(True)
+        self.treeWidget.setObjectName(_fromUtf8("treeWidget"))
+        self.treeWidget.headerItem().setText(0, _fromUtf8("1"))
+        self.verticalLayout.addWidget(self.treeWidget)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.btnAdd = QtGui.QPushButton(Dialog)
+        self.btnAdd.setObjectName(_fromUtf8("btnAdd"))
+        self.horizontalLayout.addWidget(self.btnAdd)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.btnRemove = QtGui.QPushButton(Dialog)
+        self.btnRemove.setEnabled(False)
+        self.btnRemove.setObjectName(_fromUtf8("btnRemove"))
+        self.horizontalLayout.addWidget(self.btnRemove)
+        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
+        self.btnClose = QtGui.QPushButton(Dialog)
+        self.btnClose.setObjectName(_fromUtf8("btnClose"))
+        self.horizontalLayout.addWidget(self.btnClose)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.retranslateUi(Dialog)
+        QtCore.QObject.connect(self.btnClose, QtCore.SIGNAL(_fromUtf8("clicked()")), Dialog.close)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        Dialog.setWindowTitle(_translate("Dialog", "Options Groups Manager", None))
+        self.label.setText(_translate("Dialog", "<html><head/><body><p>To see which decks use the options group, click on the plus sign next to its name.</p><p>Double clicking on the name of an options group will bring up the standard Anki dialog to change the settings (it will be opened with a dummy deck).</p><p>Double clicking on the name of a deck will let you change its options group using the standard Anki dialog.</p></body></html>", None))
+        self.btnAdd.setText(_translate("Dialog", "Add", None))
+        self.btnRemove.setText(_translate("Dialog", "Remove", None))
+        self.btnClose.setText(_translate("Dialog", "Close", None))
